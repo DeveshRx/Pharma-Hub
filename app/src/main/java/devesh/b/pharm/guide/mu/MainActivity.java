ce997f23134b79f2f2808c390450fd233029b6dd
Devesh Chaudhari <DeveshRx@users.noreply.github.com>
2020-02-18 14:06:00 +0530

    versionCode 19

--- app/src/main/java/devesh/b/pharm/guide/mu/MainActivity.java
+++ app/src/main/java/devesh/b/pharm/guide/mu/MainActivity.java
@@ -176,8 +176,8 @@ public class MainActivity extends AppCompatActivity {
 
 */
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+       // BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+      //  navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         PcognoSelectStatus = "0";
         PceuticsSelectStatus = "0";
